78438a64a39696bca42ca71b47929055b97947c8
danielg <danielg@sqreamtech.com>
2023-02-05 13:46:45 +0200

    add import

--- pysqream_blue/__init__.py
+++ pysqream_blue/__init__.py
@@ -1,3 +1,4 @@
 import os, sys
 sys.path.append(os.path.dirname(os.path.realpath(__file__)))
-from .pysqream_blue import connect
+from .pysqream_blue import connect, paramstyle
+from .utils import *
