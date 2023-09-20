61573df40796cfec186f615ade937abfe90734a4
danielg <danielg@sqreamtech.com>
2023-09-20 15:37:59 +0300

    fix

--- pysqream_blue/pysqream_blue.py
+++ pysqream_blue/pysqream_blue.py
@@ -7,10 +7,10 @@ import os
 from pysqream_blue.logger import *
 # from pysqream_blue.connection import qh_messages
 
-# os.environ['export GRPC_POLL_STRATEGY'] = 'poll'
-os.environ['export GRPC_ENABLE_FORK_SUPPORT'] = 'false'
-# os.environ['export GRPC_VERBOSITY'] = 'DEBUG'
-# os.environ['export GRPC_TRACE'] = 'all'
+# os.environ['GRPC_POLL_STRATEGY'] = 'poll'
+os.environ['GRPC_ENABLE_FORK_SUPPORT'] = 'false'
+# os.environ['GRPC_VERBOSITY'] = 'DEBUG'
+# os.environ['GRPC_TRACE'] = 'all'
 
 
 def connect(host:      str,
